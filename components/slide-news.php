5d083dd7cb207fc740302ed8e4d6d6fb49ddee20
Evgeny riadesign Pogrebnyak <riadesign@iMac-Evgeny.local>
2026-02-25 11:43:26 +1000

    добавил изображения в слайдер

--- components/slide-news.php
+++ components/slide-news.php
@@ -1,25 +1,29 @@
-        <div id="news-slide" class="slider__item">
-            <div class="index-page-slider-info">
-                <div class="container index-page-slider-info__wrapper">
-                    <div class="index-page-slider-info__title">
-                        <div class="index-page-slider-info__title-number">03</div>
-                        <div class="index-page-slider-info__title-text">Новости</div>
-                    </div>
-                    <div class="index-page-slider-info__description">
-                        <?php echo get_field('vsz_slide_news_description',11);?>
-                    </div>
-                    <div class="index-page-slider-info__buttons">
-                        <a class="index-page-slider-info__button" href="#">подробнее</a>
-                    </div>
-                </div>
+<?php 
+    $slide_image = get_field('vsz_slide_news_image',11);
+?>  
+
+<div id="news-slide" class="slider__item">
+    <div class="index-page-slider-info">
+        <div class="container index-page-slider-info__wrapper">
+            <div class="index-page-slider-info__title">
+                <div class="index-page-slider-info__title-number">03</div>
+                <div class="index-page-slider-info__title-text">Новости</div>
             </div>
-            <div class="index-page-slider__picture">
-                <div class="index-page-slider-image index-page-slider__small-image"
-                     style="--background: url('../../assets/images/img-1.jpg')"></div>
-                <div class="index-page-slider-image index-page-slider__delimiter" style="--background: url('../../assets/images/img-1.jpg')"></div>
-                <div class="index-page-slider-image index-page-slider__medium-image"
-                     style="--background: url('../../assets/images/img-1.jpg')"></div>
-                <div class="index-page-slider-image index-page-slider__main-image"
-                     style="--background: url('../../assets/images/img-1.jpg')"></div>
+            <div class="index-page-slider-info__description">
+                <?php echo get_field('vsz_slide_news_description',11);?>
+            </div>
+            <div class="index-page-slider-info__buttons">
+                <a class="index-page-slider-info__button" href="#">подробнее</a>
             </div>
         </div>
+    </div>
+    <div class="index-page-slider__picture">
+        <div class="index-page-slider-image index-page-slider__small-image"
+                style="--background: url('<?=$slide_image;?>')"></div>
+        <div class="index-page-slider-image index-page-slider__delimiter" style="--background: url('<?=$slide_image;?>')"></div>
+        <div class="index-page-slider-image index-page-slider__medium-image"
+                style="--background: url('<?=$slide_image;?>')"></div>
+        <div class="index-page-slider-image index-page-slider__main-image"
+                style="--background: url('<?=$slide_image;?>')"></div>
+    </div>
+</div>
--- components/slide-our-objects.php
+++ components/slide-our-objects.php
@@ -1,25 +1,28 @@
-        <div id="our-objects-slide" class="slider__item">
-            <div class="index-page-slider-info">
-                <div class="container index-page-slider-info__wrapper">
-                    <div class="index-page-slider-info__title">
-                        <div class="index-page-slider-info__title-number">02</div>
-                        <div class="index-page-slider-info__title-text">наши объекты</div>
-                    </div>
-                    <div class="index-page-slider-info__description">
-                        <?php echo get_field('vsz_slide_our_objects_description',11);?>
-                    </div>
-                    <div class="index-page-slider-info__buttons">
-                        <a class="index-page-slider-info__button" href="our-objects">подробнее</a>
-                    </div>
-                </div>
+<?php 
+    $slide_image = get_field('vsz_slide_our_objects_image',11);
+?>          
+<div id="our-objects-slide" class="slider__item">
+    <div class="index-page-slider-info">
+        <div class="container index-page-slider-info__wrapper">
+            <div class="index-page-slider-info__title">
+                <div class="index-page-slider-info__title-number">02</div>
+                <div class="index-page-slider-info__title-text">наши объекты</div>
             </div>
-            <div class="index-page-slider__picture">
-                <div class="index-page-slider-image index-page-slider__small-image"
-                     style="--background: url('../../assets/images/img-4.jpg')"></div>
-                <div class="index-page-slider-image index-page-slider__delimiter" style="--background: url('../../assets/images/img-4.jpg')"></div>
-                <div class="index-page-slider-image index-page-slider__medium-image"
-                     style="--background: url('../../assets/images/img-4.jpg')"></div>
-                <div class="index-page-slider-image index-page-slider__main-image"
-                     style="--background: url('../../assets/images/img-4.jpg')"></div>
+            <div class="index-page-slider-info__description">
+                <?php echo get_field('vsz_slide_our_objects_description',11);?>
+            </div>
+            <div class="index-page-slider-info__buttons">
+                <a class="index-page-slider-info__button" href="our-objects">подробнее</a>
             </div>
         </div>
+    </div>
+    <div class="index-page-slider__picture">
+        <div class="index-page-slider-image index-page-slider__small-image"
+                style="--background: url('<?=$slide_image;?>')"></div>
+        <div class="index-page-slider-image index-page-slider__delimiter" style="--background: url('<?=$slide_image;?>')"></div>
+        <div class="index-page-slider-image index-page-slider__medium-image"
+                style="--background: url('<?=$slide_image;?>')"></div>
+        <div class="index-page-slider-image index-page-slider__main-image"
+                style="--background: url('<?=$slide_image;?>')"></div>
+    </div>
+</div>
